9da50ecf84471bc017cf0719bbec0333c1ca5fb4
carsonep <carsonepeterson@gmail.com>
2022-01-18 10:57:25 -0600

    Discount API Setup with PostgreSQL Moving on

--- src/Services/Discount/Discount.API/Extensions/HostExtensions.cs
+++ src/Services/Discount/Discount.API/Extensions/HostExtensions.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Npgsql;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,14 +24,52 @@ namespace Discount.API.Extensions
 
                 try
                 {
+                    logger.LogInformation("Migrating PostgreSQL Database");
+
+                    using var connection = new NpgsqlConnection
+                        (configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+
+                    connection.Open();
+
+                    using var command = new NpgsqlCommand
+                    {
+                        Connection = connection
+                    };
+
+                    command.CommandText = "DROP TABLE IF EXISTS Coupon";
+                    command.ExecuteNonQuery();
+
+                    command.CommandText = @"CREATE TABLE Coupon(Id SERIAL PRIMARY KEY,
+                                                                ProductName VARCHAR(24) NOT NULL,
+                                                                Description TEXT,
+                                                                Amount INT)";
+
+                    command.ExecuteNonQuery();
+
+                    command.CommandText = "INSERT INTO Coupon(ProductName, Description, Amount) VALUES('IPhone X', 'IPhone Discount', 150);";
+                    command.ExecuteNonQuery();
+
+                    command.CommandText = "INSERT INTO Coupon(ProductName, Description, Amount) VALUES('Samsung 10', 'Samsung Discount', 100);";
+                    command.ExecuteNonQuery();
+
+                    logger.LogInformation("Migrated PostgreSQL Database");
 
                 }
-                catch (Exception)
+                catch (NpgsqlException ex)
                 {
 
-                    throw;
+                    logger.LogError(ex, "An error occurred while migrating to the Postgresql Database");
+
+                    if (retryForAvailability < 50)
+                    {
+                        retryForAvailability++;
+                        System.Threading.Thread.Sleep(2000);
+                        MigrateDatabase<TContext>(host, retryForAvailability);
+                    }
                 }
             }
+
+            return host;
         }
     }
 }
